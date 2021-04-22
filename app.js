2e938ebef6e833fb2fe1004bb20e2bac44b45662
monsteradurm <acranch@liquidanimation.com>
2021-04-23 04:03:08 +1000

    convert to node app

---
+++ app.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const port = 80;
+const express = require('express');
+const httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({});
+var app = express();
+
+proxy.on('error', function(e) {
+    console.log(e);
+});
+
+fs.readFile('proxy.conf.json', (err, data) => {
+    if (err) throw err;
+    var config = JSON.parse(data);
+
+    //console.log(config);
+
+    Object.keys(config).forEach(addr => {
+        console.log("Setting proxy --> " + addr, '--> '+  config[addr].target);
+
+        app.all(addr, (req, res) => {
+            let arr = req.url.split('/');
+            arr.splice(0, 2);
+
+            req.url = '/' + arr.join('/');
+
+            console.log(req.url);
+
+            proxy.web(req, res, {
+                
+                target: config[addr].target,
+                https: true,
+                changeOrigin: true
+              });
+        })
+    });
+
+    app.use(express.static(__dirname+'/dist/project-mgr2'));
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname+'/dist/project-mgr2/index.html'));
+    });
+
+    app.listen(port, () => {
+        console.log("project-mgr2 --> listening at: " + port)
+    })
+});
